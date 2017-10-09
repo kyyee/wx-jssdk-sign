4d45a55ab75354b84e898910dd9fdc576a53d1df
kyyee <kyyeeyoung@163.com>
2017-10-09 23:50:34 +0800

    1.修改前端app.js中的wx参数注入方式

--- src/main/resources/static/script/app.js
+++ src/main/resources/static/script/app.js
@@ -14,110 +14,291 @@
  * 邮件主题：【微信JS-SDK反馈】具体问题
  * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
  */
-(function () {
+function wxShare() {
     let url = window.location.href;
-    $.get('sign?url=' + url, function (data, textStatus, jqXHR) {
-        console.log('数据：' + JSON.stringify(data) + '\n状态：' + textStatus + '\n响应头：' + JSON.stringify(jqXHR));
-        let success = status === 'success' && data.result >= 0;
-        if (success) {
-            let sign = data.data;
-            console.log('sign：' + JSON.stringify(sign));
+    let configs = {
+        debug: false,
+        appId: 'wxee843bb809c9f926',
+        timestamp: "",
+        nonceStr: "",
+        signature: "",
+        title: window.document.title,
+        desc: window.location.host,
+        link: url,
+        imgUrl: ''
+    };
 
-            wx.config({
-                debug: true,                // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                appId: sign.appId,          // 必填，公众号的唯一标识
-                timestamp: sign.timestamp, // 必填，生成签名的时间戳
-                nonceStr: sign.nonceStr,   // 必填，生成签名的随机串
-                signature: sign.signature, // 必填，签名，见附录1
-                jsApiList: [                 // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-                    // 所有要调用的 API 都要加到这个列表中
-                    'onMenuShareTimeline',
-                    'onMenuShareAppMessage',
-                    'onMenuShareQQ',
-                    'onMenuShareQZone',
-                    'getNetworkType',
-                    'getLocation'
-                ]
-            });
-
-            wx.ready(function () {
-                // 处理成功验证，在这里调用 API
-                // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-                // 分享到朋友圈
-                wx.onMenuShareTimeline({
-                    title: '标题',              // 分享标题
-                    link: url,   // 分享链接，该链接域名或路径必须与当前页面对应的公众号js安全域名一致
-                    imgUrl: '',                 // 分享图标
-                    success: function () {     // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {      // 用户取消分享后执行的回调函数
-                    }
-                });
-
-                // 发送给朋友
-                wx.onMenuShareAppMessage({
-                    title: '标题',               // 分享标题
-                    desc: '测试分享给朋友',      // 分享描述
-                    link: url,    // 分享链接，该链接域名或路径必须与当前页面对应的公众号js安全域名一致
-                    imgUrl: '',                  // 分享图标
-                    type: '',                    // 分享类型，music、video、link，不填默认为link
-                    dataUrl: '',                 // 如果type为music或video，则要提供数据链接，默认为空
-                    success: function () {      // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {       // 用户取消分享后执行的回调函数
-                    }
-                });
-
-                // 分享到QQ
-                wx.onMenuShareQQ({
-                    title: '标题',               // 分享标题
-                    desc: '测试分享给朋友',      // 分享描述
-                    link: url,    // 分享链接，该链接域名或路径必须与当前页面对应的公众号js安全域名一致
-                    imgUrl: '',                  // 分享图标
-                    success: function () {      // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {       // 用户取消分享后执行的回调函数
-                    }
-                });
-
-                // 分享到QQ空间
-                wx.onMenuShareQZone({
-                    title: '标题',               // 分享标题
-                    desc: '测试分享给朋友',      // 分享描述
-                    link: url,    // 分享链接，该链接域名或路径必须与当前页面对应的公众号js安全域名一致
-                    imgUrl: '',                  // 分享图标
-                    success: function () {      // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {       // 用户取消分享后执行的回调函数
-                    }
-                });
-
-                // 获取网络状态接口
-                wx.getNetworkType({
-                    success: function (res) {
-                        let networkType = res.networkType; // 返回网络类型 2G、3G、4G、WIFI
-                        console.log('网络状态为：' + networkType);
-                    }
-                });
-
-                // 获取地理位置接口
-                wx.getLocation({
-                    type: 'wgs84',                     // 默认为 wgs84 的 gps 坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-                    success: function (res) {
-                        let latitude = res.latitude;   // 纬度，浮点数，范围为90 — -90
-                        let longitude = res.longitude; // 经度，浮点数，范围为180 — -180
-                        let speed = res.speed;         // 速度，以 m/s 计
-                        let accuracy = res.accuracy;   // 位置精度
-                        console.log('地理纬度：' + latitude + '经度：' + longitude + '速度：' + speed + '位置精度：' + accuracy);
-                    }
-                });
-            });
-
-            wx.error(function (res) {
-                // 处理失败验证
-                // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-                alert('签名验证失败！');
-            });
+    this.share = function (options) {
+        if (options) {
+            $.extend(configs, options);
         }
-    }, 'json');
-})();
+
+        if (!configs.signature) {
+            $.get('sign?url=' + encodeURIComponent(url), function (data, textStatus, jqXHR) {
+                console.log('数据：' + JSON.stringify(data) + '\n状态：' + textStatus + '\n响应头：' + JSON.stringify(jqXHR));
+                let success = status === 'success' && data.result >= 0;
+                if (success) {
+                    console.log('sign：' + JSON.stringify(data.data));
+                    $.extend(configs, data.data);
+                    config();
+                    ready();
+                    error();
+                }
+            });
+        } else {
+            config();
+            ready();
+            error();
+        }
+    };
+
+    function config() {
+        wx.config({
+            debug: configs.debug, //开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端 打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: configs.appId, // 必填，公众号的唯一标识
+            timestamp: configs.timestamp, // 必填，生成签名的时间戳
+            nonceStr: configs.nonceStr, // 必填，生成签名的随机串
+            signature: configs.signature,// 必填，签名，见附录1
+            jsApiList: [
+                'checkJsApi',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage',
+                'onMenuShareQQ',
+                'onMenuShareWeibo',
+                'hideMenuItems',
+                'showMenuItems',
+                'hideAllNonBaseMenuItem',
+                'showAllNonBaseMenuItem',
+                'translateVoice',
+                'startRecord',
+                'stopRecord',
+                'onRecordEnd',
+                'playVoice',
+                'pauseVoice',
+                'stopVoice',
+                'uploadVoice',
+                'downloadVoice',
+                'chooseImage',
+                'previewImage',
+                'uploadImage',
+                'downloadImage',
+                'getNetworkType',
+                'openLocation',
+                'getLocation',
+                'hideOptionMenu',
+                'showOptionMenu',
+                'closeWindow',
+                'scanQRCode',
+                'chooseWXPay',
+                'openProductSpecificView',
+                'addCard',
+                'chooseCard',
+                'openCard'
+            ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
+    }
+
+    function ready() {
+        wx.ready(function () {
+            wx.onMenuShareAppMessage({
+                title: configs.title,
+                desc: configs.desc,
+                link: configs.link,
+                imgUrl: configs.imgUrl,
+                trigger: function (res) {
+                },
+                success: function (res) {
+                },
+                cancel: function (res) {
+                },
+                fail: function (res) {
+                }
+            });
+
+            wx.onMenuShareTimeline({
+                title: configs.title,
+                desc: configs.desc,
+                link: configs.link,
+                imgUrl: configs.imgUrl,
+                trigger: function (res) {
+                },
+                success: function (res) {
+                },
+                cancel: function (res) {
+                },
+                fail: function (res) {
+                }
+            });
+
+            wx.onMenuShareQQ({
+                title: configs.title,
+                desc: configs.desc,
+                link: configs.link,
+                imgUrl: configs.imgUrl,
+                trigger: function (res) {
+                },
+                success: function (res) {
+                },
+                cancel: function (res) {
+                },
+                fail: function (res) {
+                }
+            });
+
+            // 分享到QQ空间
+            wx.onMenuShareQZone({
+                title: '标题',               // 分享标题
+                desc: '测试分享给朋友',      // 分享描述
+                link: url,    // 分享链接，该链接域名或路径必须与当前页面对应的公众号js安全域名一致
+                imgUrl: '',                  // 分享图标
+                trigger: function (res) {
+                },
+                success: function (res) {
+                },
+                cancel: function (res) {
+                },
+                fail: function (res) {
+                }
+            });
+
+            wx.onMenuShareWeibo({
+                title: configs.title,
+                desc: configs.desc,
+                link: configs.link,
+                imgUrl: configs.imgUrl,
+                trigger: function (res) {
+                },
+                success: function (res) {
+                },
+                cancel: function (res) {
+                },
+                fail: function (res) {
+                }
+            });
+        });
+    }
+
+    function error() {
+        wx.error(function (res) {
+            alert(res.errMsg);
+        });
+    }
+}
+
+$(function () {
+    new wxShare().share({
+        desc: '测试页面'
+    });
+});
+
+// (function () {
+//     let url = window.location.href;
+//     let title = window.document.title;
+//     let desc = window.location.host;
+//     let imgUrl = '';
+//     $.get('sign?url=' + url, function (data, textStatus, jqXHR) {
+//         console.log('数据：' + JSON.stringify(data) + '\n状态：' + textStatus + '\n响应头：' + JSON.stringify(jqXHR));
+//         let success = status === 'success' && data.result >= 0;
+//         if (success) {
+//             let sign = data.data;
+//             console.log('sign：' + JSON.stringify(sign));
+//
+//             let wx;
+//             wx.config({
+//                 debug: true,                // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+//                 appId: sign.appId,          // 必填，公众号的唯一标识
+//                 timestamp: sign.timestamp, // 必填，生成签名的时间戳
+//                 nonceStr: sign.nonceStr,   // 必填，生成签名的随机串
+//                 signature: sign.signature, // 必填，签名，见附录1
+//                 jsApiList: [                 // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+//                     // 所有要调用的 API 都要加到这个列表中
+//                     'onMenuShareTimeline',
+//                     'onMenuShareAppMessage',
+//                     'onMenuShareQQ',
+//                     'onMenuShareQZone',
+//                     'getNetworkType',
+//                     'getLocation'
+//                 ]
+//             });
+//
+//             wx.ready(function () {
+//                 // 处理成功验证，在这里调用 API
+//                 // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+//                 // 分享到朋友圈
+//                 wx.onMenuShareTimeline({
+//                     title: '标题',              // 分享标题
+//                     link: url,   // 分享链接，该链接域名或路径必须与当前页面对应的公众号js安全域名一致
+//                     imgUrl: '',                 // 分享图标
+//                     success: function () {     // 用户确认分享后执行的回调函数
+//                     },
+//                     cancel: function () {      // 用户取消分享后执行的回调函数
+//                     }
+//                 });
+//
+//                 // 发送给朋友
+//                 wx.onMenuShareAppMessage({
+//                     title: '标题',               // 分享标题
+//                     desc: '测试分享给朋友',      // 分享描述
+//                     link: url,    // 分享链接，该链接域名或路径必须与当前页面对应的公众号js安全域名一致
+//                     imgUrl: '',                  // 分享图标
+//                     type: '',                    // 分享类型，music、video、link，不填默认为link
+//                     dataUrl: '',                 // 如果type为music或video，则要提供数据链接，默认为空
+//                     success: function () {      // 用户确认分享后执行的回调函数
+//                     },
+//                     cancel: function () {       // 用户取消分享后执行的回调函数
+//                     }
+//                 });
+//
+//                 // 分享到QQ
+//                 wx.onMenuShareQQ({
+//                     title: '标题',               // 分享标题
+//                     desc: '测试分享给朋友',      // 分享描述
+//                     link: url,    // 分享链接，该链接域名或路径必须与当前页面对应的公众号js安全域名一致
+//                     imgUrl: '',                  // 分享图标
+//                     success: function () {      // 用户确认分享后执行的回调函数
+//                     },
+//                     cancel: function () {       // 用户取消分享后执行的回调函数
+//                     }
+//                 });
+//
+//                 // 分享到QQ空间
+//                 wx.onMenuShareQZone({
+//                     title: '标题',               // 分享标题
+//                     desc: '测试分享给朋友',      // 分享描述
+//                     link: url,    // 分享链接，该链接域名或路径必须与当前页面对应的公众号js安全域名一致
+//                     imgUrl: '',                  // 分享图标
+//                     success: function () {      // 用户确认分享后执行的回调函数
+//                     },
+//                     cancel: function () {       // 用户取消分享后执行的回调函数
+//                     }
+//                 });
+//
+//                 // 获取网络状态接口
+//                 wx.getNetworkType({
+//                     success: function (res) {
+//                         let networkType = res.networkType; // 返回网络类型 2G、3G、4G、WIFI
+//                         console.log('网络状态为：' + networkType);
+//                     }
+//                 });
+//
+//                 // 获取地理位置接口
+//                 wx.getLocation({
+//                     type: 'wgs84',                     // 默认为 wgs84 的 gps 坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+//                     success: function (res) {
+//                         let latitude = res.latitude;   // 纬度，浮点数，范围为90 — -90
+//                         let longitude = res.longitude; // 经度，浮点数，范围为180 — -180
+//                         let speed = res.speed;         // 速度，以 m/s 计
+//                         let accuracy = res.accuracy;   // 位置精度
+//                         console.log('地理纬度：' + latitude + '经度：' + longitude + '速度：' + speed + '位置精度：' + accuracy);
+//                     }
+//                 });
+//             });
+//
+//             wx.error(function (res) {
+//                 // 处理失败验证
+//                 // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+//                 alert(res.errMsg);
+//             });
+//         }
+//     }, 'json');
+// })();
